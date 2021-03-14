22bbd16e06ce80227794f7f9fe0a4a42887c92fd
mendoza2929 <reuelmendoza29@gmail.com>
2021-03-14 20:20:54 +0800

    Update Login.js

--- amtech/src/Login.js
+++ amtech/src/Login.js
@@ -1,10 +1,14 @@
-import React from 'react'
+
 import './Login.css'
 import {Link } from "react-router-dom";
 
 
 
 function Login() {
+
+    
+
+
     return (
         <div className='login'>
             <Link to ='/'>
